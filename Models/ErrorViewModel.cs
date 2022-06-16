eb578260fe6a02647a4ff47449c71ce22b2b67df
Akbarali19 <sutharakbarali5@gmail.com>
2022-06-16 21:43:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoonMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
